4aef43cf2a593033a7c1f92ea72edc9c3bb032da
Me21cr <128864515+Me21cr@users.noreply.github.com>
2023-08-04 01:52:02 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { useState } from "react";
+import React (useState,useEffect) from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const[counter,setCounter] = useState(0)
+	let contador = 0;
+	let suma =(a) => {
+		contador= a+1;
+		console.log("varaible a=" ,a)
+		return contador;	
+	}
+	const sumaEstadoCounter =(a) => {
+		setCounter (a+1)
+		return counter
+	}
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
@@ -19,6 +31,8 @@ const Home = () => {
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+			<button onClick={(evento)=>{suma(contador)}}>Suma</button>
+			<h1>El valor del contador es {contador}</h1>
 		</div>
 	);
 };
